8dedf98fc7193939b2685c9a558436df0a39dff3
Christian BRUNON <59100130+topbestcoder@users.noreply.github.com>
2021-11-16 18:34:13 +0100

    Delete CrystalGalaxy/levels directory

--- CrystalGalaxy/levels/Survival.js
+++
@@ -1,1 +0,0 @@
-Effect.Game.ll.nl({Code:0,LevelID:"Survival",Data:{layers:{sprites:[{id:"player",zIndex:3,type:"Ship",width:64,height:74,x:352,y:255}]},next_sprite_id:2}});
--- CrystalGalaxy/levels/TitleScreen.js
+++
@@ -1,1 +0,0 @@
-Effect.Game.ll.nl({Code:0,LevelID:"TitleScreen",Data:{}});
